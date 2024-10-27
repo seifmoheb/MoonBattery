44cec4bc1dc355ad2b025692991b73920faf81fd
seifmoheb <smoheb33@gmail.com>
2024-10-27 20:00:36 +0300

    Adding Configuration endpoint

--- app/controllers/configuration_controller.rb
+++ app/controllers/configuration_controller.rb
@@ -1,2 +1,23 @@
 class ConfigurationController < ApplicationController
+    protect_from_forgery with: :null_session
+
+    def add
+        configs = params[:configurations]
+        macaddress_received = params[:macaddress]
+        configs = configs.split(';',-1)
+        configs.each do |item|
+            pair = item.split(':',-1)
+            key_received = pair[0]
+            value_received = pair[1]
+            existingConfig = ::Configuration.find_by(:macaddress => macaddress_received, :configuration => key_received)
+            if existingConfig.nil?
+                # no configuration of this type for this macaddress
+                @newConfigRecord = ::Configuration.new(macaddress:macaddress_received,configuration:key_received,value:value_received)
+                @newConfigRecord.save!
+            else
+                existingConfig.update(value:value_received)
+                #An exisiting record for this configuration
+            end
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post 'storage/create' => 'storage#create'
   root "home#index"
   get '/ping', to: 'storage#ping'
+  post 'configuration/add', to:'configuration#add'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
